2d7e572d23de559e561be50c6f05f3086355f0b0
JP Lyon <japylyon@gmail.com>
2024-12-29 09:08:40 -0600

    Day 4 Solutions

---
+++ Day 4/Day 4 pt. 2.py
@@ -0,0 +1,14 @@
+import hashlib
+
+key = "iwrupvqb"
+num = 0
+hash_key = key + str(num)
+hash_object = hashlib.md5(hash_key.encode())
+hex_digest = hash_object.hexdigest()
+while hex_digest[0:6] != "000000":
+    num += 1
+    hash_key = key + str(num)
+    hash_object = hashlib.md5(hash_key.encode())
+    hex_digest = hash_object.hexdigest()
+
+print(num)
