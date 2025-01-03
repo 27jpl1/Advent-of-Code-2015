667e1dfe9f7592b5b640ba5e5051105801c6379c
JP Lyon <japylyon@gmail.com>
2025-01-03 08:27:02 -0600

    Day 9 Solutions

---
+++ Day 9/Day 9 pt. 2.py
@@ -0,0 +1,37 @@
+from itertools import permutations
+
+file = open("Paths", "r")
+
+edges = {}
+for line in file:
+    line = line.strip().split()
+    start = line[0]
+    end = line[2]
+    weight = line[4]
+    if start in edges.keys():
+        edges[start].append((end, weight))
+    else:
+        edges[start] = [(end, weight)]
+    if end in edges.keys():
+        edges[end].append((start, weight))
+    else:
+        edges[end] = [(start, weight)]
+
+cities = []
+for key in edges.keys():
+    cities.append(key)
+
+possibilities = list(permutations(cities))
+
+largest = 0
+for possibility in possibilities:
+    i = 0
+    total = 0
+    while i < len(possibility) - 1:
+        for pair in edges[possibility[i]]:
+            if pair[0] == possibility[i + 1]:
+                total += int(pair[1])
+        i += 1
+    if total > largest:
+        largest = total
+print(largest)
